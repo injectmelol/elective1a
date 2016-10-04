1523b60e1e8c8cb8911a5e982a901c98542b7da1
paranz <injectmelol@gmail.com>
2016-10-04 15:48:56 +0800

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>CATFIS | HOME</title>
+		<link rel="stylesheet" type="text/css" href="styles/index.css">
+	</head>
+	<body>
+		<header>
+			<a href="">CATFIS</a>
+		</header>
+	</body>
+</html>
